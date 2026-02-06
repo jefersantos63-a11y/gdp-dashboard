22e2e90b869c303ce7d9d8f7940a2530d1af3c65
jefersantos63-a11y <jefersantos63@gmail.com>
2026-02-06 15:42:14 -0500

    feat: agregar servicio de datos del PIB

---
+++ services/gdp_service.py.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pathlib import Path
+import streamlit as st
+
+@st.cache_data
+def obtener_datos_del_PIB():
+    ruta = Path(__file__).parent.parent / "data" / "datos_pib.csv"
+    df = pd.read_csv(ruta)
+
+    df = df.melt(
+        id_vars=["Country Name", "Country Code", "Indicator Name", "Indicator Code"],
+        var_name="year",
+        value_name="gdp"
+    )
+
+    df = df.dropna(subset=["gdp"])
+    df["year"] = df["year"].astype(int)
+
+    return df
