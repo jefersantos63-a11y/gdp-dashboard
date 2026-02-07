7812172e8976c4b1e15ae7304e9beb1d82e57e8c
jefersantos63-a11y <jefersantos63@gmail.com>
2026-02-06 20:20:18 -0500

    Update gdp_service.py

--- gdp_service.py
+++ gdp_service.py
@@ -1,19 +1,7 @@
-import pandas as pd
 from pathlib import Path
-
+import pandas as pd
 
 def obtener_datos_del_PIB():
-    ruta = Path(__file__).parent / "datos" / "gdp_data.csv"
+    ruta = Path(__file__).parent / "data" / "datos_pib.csv.csv"
     df = pd.read_csv(ruta)
-
-    df = df.melt(
-        id_vars=["Country Name", "Country Code", "Indicator Name", "Indicator Code"],
-        var_name="year",
-        value_name="gdp"
-    )
-
-    df = df.dropna(subset=["gdp"])
-    df["year"] = df["year"].astype(int)
-
     return df
-
